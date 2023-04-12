0dd43a459730eaa5bac9384c4dd8c49a8688119a
madelinegit <120407458+madelinegit@users.noreply.github.com>
2023-04-12 15:22:10 -0400

    Javascript & Homepage
    Add Javascript to buttons in order to  log into site and add home page

---
+++ artproject/script.js
@@ -0,0 +1,9 @@
+function login() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if (username === "" || password === "") {
+		alert("Please enter your username and password.");
+	} else {
+		// do something to log in the user
+	}
+}
